3ac4fa0b20c63803c0a05a029520a9c19c453b0a
Andrii Hnedko <andrii.hnedko@jointoit.com>
2021-04-07 12:48:19 +0300

    commit

--- App.tsx
+++ App.tsx
@@ -1,115 +1,56 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React from 'react';
+import {SafeAreaView, View} from 'react-native';
+import RNFS from 'react-native-fs';
 
- import React from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
+const App = () => {
+  RNFS.readDir(RNFS.CachesDirectoryPath)
+    .then(result => {
+      console.warn('GOT RESULT', result);
+    })
+    .catch(e => {
+      console.error(e.message);
+    });
+  RNFS.writeFile(
+    RNFS.CachesDirectoryPath + 'test.tsx',
+    'Lorem ipsum dolor sit amet',
+    'utf8',
+  )
+    .then(success => {
+      console.warn('FILE WRITTEN!');
+      RNFS.unlink(RNFS.CachesDirectoryPath + 'test.tsx')
+        .then(() => {
+          console.warn('FILE DELETED');
+        })
+        // `unlink` will throw an error, if the item to unlink does not exist
+        .catch(err => {
+          console.error(err.message);
+        });
+    })
+    .catch(err => {
+      console.error(err.message);
+    });
+  RNFS.mkdir(RNFS.CachesDirectoryPath + 'test', {
+    NSURLIsExcludedFromBackupKey: true,
+  })
+    .then(r => {
+      console.warn('DIR CREATED');
+      RNFS.unlink(RNFS.CachesDirectoryPath + 'test')
+        .then(() => {
+          console.warn('DIR DELETED');
+        })
+        // `unlink` will throw an error, if the item to unlink does not exist
+        .catch(err => {
+          console.error(err.message);
+        });
+    })
+    .catch(err => {
+      console.error(err.message);
+    });
+  return (
+    <SafeAreaView>
+      <View />
+    </SafeAreaView>
+  );
+};
 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
-
- const Section: React.FC<{
-   title: string;
- }> = ({children, title}) => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
-
- const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
-
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
-
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View
-           style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-           }}>
-           <Section title="Step One">
-             Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
-           </Section>
-           <Section title="See Your Changes">
-             <ReloadInstructions />
-           </Section>
-           <Section title="Debug">
-             <DebugInstructions />
-           </Section>
-           <Section title="Learn More">
-             Read the docs to discover what to do next:
-           </Section>
-           <LearnMoreLinks />
-         </View>
-       </ScrollView>
-     </SafeAreaView>
-   );
- };
-
- const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
- });
-
- export default App;
+export default App;
